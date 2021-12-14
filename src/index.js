4889c542ec86ae02e96846fb2a8fb5637621cbec
Chikusonu <druva186@gmail.com>
2021-12-14 16:16:45 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Nav from "./Nav";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render([<App />, <Nav />] ,document.getElementById("root"));
