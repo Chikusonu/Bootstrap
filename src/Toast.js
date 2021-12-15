5f28dcdd764a17805e4edf9c236e525e712edf64
Chikusonu <druva186@gmail.com>
2021-12-15 06:45:46 +0530

    2 files modified

--- src/Toast.js
+++ src/Toast.js
@@ -1,8 +1,17 @@
-import React from "react";
-export default function Toast () {
-  return(
+import React from 'react';
+import Toast from 'react-bootstrap/Toast';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function Toast() {
+  return (
     <div>
-      hey
-      </div>
+      <Toast>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+      </Toast>
+    </div>
   );
 }
