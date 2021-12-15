ce409026f45f6cccb8dac5027c02d81e3b1bf7ca
Chikusonu <druva186@gmail.com>
2021-12-15 06:35:14 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import Nav from "./Nav";
+import Toast from "./Toast";
 
-ReactDOM.render([<App />, <Nav />] ,document.getElementById("root"));
+ReactDOM.render([<App />, <Nav />, <Toast />] ,document.getElementById("root"));
