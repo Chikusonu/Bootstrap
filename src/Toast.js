7be0d053e0a702416ff86491aaca1cbf3750d620
Chikusonu <druva186@gmail.com>
2021-12-16 22:08:38 +0530

    2 files modified

--- src/Toast.js
+++ src/Toast.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import Toast from 'react-bootstrap/Toast';
-import Image from 'react-bootstrap/Image';
+import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Toast() {
   return (
     <div>
-      <Toast>
-        <Toast.Header>
-          <img src="https://bit.ly/3yww1FN/20x20" className="rounded me-2" alt="" thumbnail/>
-          <strong className="me-auto">Bootstrap</strong>
-          <small>11 mins ago</small>
-        </Toast.Header>
-        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-      </Toast>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPI7lfuTe0WE1h5Z8fi3JyQAie65FdC-PFmUcyXIu8V6U1hY8IczxlpJOx&s=10"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShakJf6_nd7WOFJHh6NK90P-qMV6kyEUe1DaZOaiC6Cgscj2mhEijDe6uM&s=10"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZiZfrlEAeTHV7P2t84YHN2U_zlPh1IX4Ra-A1xqQfJY_qjTmZqQ2sbwc&s=10"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
